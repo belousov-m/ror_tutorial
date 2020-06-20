71186b66fdc342984cfc6bf1b625d050cbc00b0c
lena <derlaster@yandex.ru>
2020-06-20 16:54:25 +0300

    generate Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root             'static_pages#home'
-
-  get 'home'   => 'static_pages#home'
+  
+  get 'home'    => 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'help'    => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
-
+  
+  get 'signup'  => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
