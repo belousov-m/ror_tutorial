b8bd6f0856dd0d72475e4d4377c6b137badad3c4
lena <derlaster@yandex.ru>
2020-06-20 16:50:23 +0300

    set new url after update in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root             'static_pages#home'
 
+  get 'home'   => 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'help'    => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
