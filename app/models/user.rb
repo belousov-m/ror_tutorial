a0aa4101226d3dc159189438f0f73b2725dcfe7c
lena <derlaster@yandex.ru>
2021-01-03 23:12:31 +0300

    rubocop fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase}
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: {case_sensitive: false}
